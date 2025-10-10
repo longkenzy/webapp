e85b3eb4f3b9fa2e66de83b9da30489798785b0f
Long KenZy <it.htlong25@gmail.com>
2025-10-10 11:50:05 +0700

    Sửa hydration error và cleanup tài liệu tạm
    - Thêm suppressHydrationWarning vào body tag
    
    - Fix lỗi browser extension inject attributes
    
    - Xóa các file markdown tài liệu tạm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <Providers>
           <GlobalErrorHandler />
